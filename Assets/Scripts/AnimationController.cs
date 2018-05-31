d16bc58d9a95e20b31ace3b869a0ed5f0f4c90cc
Heyapple0 <36547907+Heyapple0@users.noreply.github.com>
2018-05-31 10:51:30 +0300

    git add

---
+++ Assets/Scripts/AnimationController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour {
+
+    private Animator anim;
+    public string triggerToSet;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    public void SetTrigger()
+    {
+        anim.SetTrigger(triggerToSet);
+    }
+}
